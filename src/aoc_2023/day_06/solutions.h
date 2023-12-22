f6db3bc5710784586ef90f9ec27b148c68d4f3c5
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2023-12-22 11:46:45 +0000

    solve day 06 part 2

--- src/aoc_2023/day_06/solutions.h
+++ src/aoc_2023/day_06/solutions.h
@@ -6,14 +6,18 @@
 #define MAX_RACES 4
 
 typedef struct Race {
-    int time;
-    int record_dist;
+    long long int time;
+    long long int record_dist;
 } Race;
 
 void parse_races(char const *filename, Race *races, size_t *races_count);
 
-int count_ways_to_win(Race race);
+long long int count_ways_to_win(Race race);
 
 int solve_part_one(char const *filename);
+
+Race parse_single_race(char const *filename);
+
+long long int solve_part_two(char const *filename);
 
 #endif  // SOLUTIONS_H
