cc12e1137b63ed2cce4b62e51c8fb39af404792a
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2023-12-22 09:40:36 +0000

    solve day 06 part 1

---
+++ src/aoc_2023/day_06/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+
+#include "solutions.h"
+
+int main(void) {
+    printf("--- Day 6: Wait For It ---\n");
+
+    clock_t start;
+    clock_t end;
+
+    start = clock();
+    printf("Answer for part 1: %u ", solve_part_one("input.txt"));
+    end = clock();
+    printf("(took %.4f seconds)\n", (double)(end - start) / CLOCKS_PER_SEC);
+
+    // start = clock();
+    // printf("Answer for part 2: %d ", solve_part_two("input.txt"));
+    // end = clock();
+    // printf("(took %.4f seconds)\n", (double)(end - start) / CLOCKS_PER_SEC);
+}
